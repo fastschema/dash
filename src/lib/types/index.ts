2e24497cdf9522428118ab424b09503551a08b35
Ngoc Phuong <nguyenngocphuongnb@gmail.com>
2024-04-03 12:14:05 +0700

    Fix standalone route error by enable next trailingSlash

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -5,4 +5,4 @@ export * from './app';
 
 export const API_BASE_URI = process.env.NEXT_PUBLIC_API_BASE_URI;
 export const COOKIE_NAME = process.env.NEXT_PUBLIC_COOKIE_NAME ?? 'token';
-export const standaloneRoutes = ['/login', '/register', '/activate', '/setup'];
+export const standaloneRoutes = ['/login/', '/register/', '/activate/', '/setup/'];
